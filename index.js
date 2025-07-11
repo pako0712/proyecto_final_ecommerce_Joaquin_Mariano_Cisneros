64f6f892b969252ee194698e017925847bef7c84
pako0712 <joacisneros@yahoo.com.ar>
2025-07-11 12:42:24 -0300

    agregado de carpetas y gitkeep. En index ruta a products

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
 const app = express();
+
+const products=[{id:1, name:"banana", price:20},
+    {id:2, name:"manzana", price:100},
+        {id:3, name:"uva", price:75},
+]
 app.get("/", (req, res)=>{
     res.send("aca esta el servidor funcando!");
+})
+
+app.get("/products", (req, res)=>{
+    res.json(products);
 })
 
 const PORT = 3000;
